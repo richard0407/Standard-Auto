bcb43a837311cc8eb838dcbe3fe9cf3a316f1922
richard0407 <103963142+richard0407@users.noreply.github.com>
2022-05-31 21:56:22 -0400

    Update HighballGoal.java

--- src/main/java/frc/robot/subsystems/turret/commands/HighballGoal.java
+++ src/main/java/frc/robot/subsystems/turret/commands/HighballGoal.java
@@ -1,1 +1,24 @@
+package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.turret.Turret;
+
+public class HighballGoal extends Commandbase{
+  private Turret turret;
+  private double shootAngle;
+  private double shootSpeed;
+  
+  public HighballGoal(Turret turret, double shootAngle, double shootSpeed) {
+    this.turret = turret;
+    this.shootAngle = shootAngle;
+    this.shootSpeed = shootAngle;
+    addRequirements(turret);
+  }
+  @Override
+  public void execute(){
+    if(shootAngle == highgoal){
+      this.speedMotor.set(double Container.kHighBallToGoalSpeed);
+    } 
+  }
+  
+}
