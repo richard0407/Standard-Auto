6d27baaed143cb055ac3c3166450e832c06d1d4c
richard0407 <103963142+richard0407@users.noreply.github.com>
2022-05-31 21:02:22 -0400

    Update and rename turret.java to Turret.java

---
+++ src/main/java/frc/robot/subsystems/turret/Turret.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems.intake;
+
+import com.kauailabs.navx.frc.AHRS;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMax.IdleMode;
+
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants; 
