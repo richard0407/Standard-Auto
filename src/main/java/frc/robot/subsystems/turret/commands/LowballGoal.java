be47fa9b76e79b7341aee117911512bfcf541978
richard0407 <103963142+richard0407@users.noreply.github.com>
2022-05-31 22:48:56 -0400

    Update LowballGoal.java

--- src/main/java/frc/robot/subsystems/turret/commands/LowballGoal.java
+++ src/main/java/frc/robot/subsystems/turret/commands/LowballGoal.java
@@ -1,1 +1,42 @@
+package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.turret.Turret;
+
+public class LowballGoal extends Commandbase{
+  private Turret turret;
+  private double shootAngle;
+  private boolean ballShot;
+  private double lowGoalbig;
+  private double lowGoalSmall;
+  private double shotFinished;
+  
+  public LowballGoal(Turret turret, double shootAngle) {
+    this.turret = turret;
+    this.shootAngle = shootAngle;
+    this.ballShot = false;
+    this.lowGoalBig = 0;
+    this.lowGoalSmall = 45;
+    this.shotFinished = 1.0; 
+    addRequirements(turret);
+  }
+  @Override
+  public void execute(){
+    if(shootAngle < lowGoalBig && shootAngle > lowGoalSmall){
+      this.turret.shootPower(double Container.kLowBallToGoalSpeed);
+    }else{
+      boolean ballShot = true;
+    } 
+  }
+  @Override 
+  public boolean isFinished(){
+    if(shootFinished != 0.0){
+      boolean ballShot = false;
+    }
+    return ballShot;
+  }
+  @Override
+  public void end(boolean interrupted){
+    this.turret.shootPower(0.0);
+  }
+}
